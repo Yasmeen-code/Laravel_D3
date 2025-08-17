2659dfae5a6cac709861feea9eff9324c373c5a1
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-17 13:52:44 +0300

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -8,12 +8,20 @@
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
+                    
                 </div>
+                
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                          <x-nav-link :href="route('posts.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('All Posts') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('posts.create')" :active="request()->routeIs('dashboard')">
+                        {{ __('Create Post') }}
                     </x-nav-link>
                 </div>
             </div>
