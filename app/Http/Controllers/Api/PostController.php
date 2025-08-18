e601863d06431491585110a5c91f7d27e42031f4
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-18 15:46:44 +0300

    first commit

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
 use App\Http\Resources\PostResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 use function Pest\Laravel\post;
